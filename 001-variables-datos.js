addd00ec5d6cdfff30e7c37d5d8acb65b02d0237
estrelladev <gujo716@gmail.com>
2024-07-13 15:50:28 -0400

    first files

---
+++ 001-variables-datos.js
@@ -0,0 +1,17 @@
+//String
+let name = "Estrella";
+
+//Numeros
+let num1 = 5;
+let num2 = -5;
+let num3 = 5.1;
+
+//Boleanos
+let imON = true;
+let imOff = false;
+
+//Undefined
+let aunNo;
+
+//Null
+let aunEmpty = null;
